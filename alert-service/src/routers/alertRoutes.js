78e2eed2bd0e848f951277d79639a5b66933e0ee
jose6668 <jlvargas-2022b@corhuila.edu.co>
2026-03-31 16:33:45 -0500

    feat(HU-003-qa) A new method for generating alerts for expired products was implemented.

--- alert-service/src/routers/alertRoutes.js
+++ alert-service/src/routers/alertRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
+const expiredAlertController = require("../controllers/expiredAlertController");
 const lowStockAlertController = require("../controllers/lowStockAlertController");
 
 const router = Router();
 
+router.get("/api/alerts/expired", expiredAlertController.getExpiredAlerts);
 router.get("/api/alerts/low-stock", lowStockAlertController.getLowStockAlerts);
 
 module.exports = router;

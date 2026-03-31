bd23c735246460fe53ac2905688dd0db0898c597
jose6668 <jlvargas-2022b@corhuila.edu.co>
2026-03-31 16:24:23 -0500

    feat(HU-002-qa) A new method was implemented to generate alerts for products with stock below the minimum level.

---
+++ alert-service/src/config/constants.js
@@ -0,0 +1,14 @@
+const ALERT_TYPES = {
+  LOW_STOCK: "LOW_STOCK",
+};
+
+const ALERT_SEVERITIES = {
+  HIGH: "HIGH",
+  MEDIUM: "MEDIUM",
+  LOW: "LOW",
+};
+
+module.exports = {
+  ALERT_TYPES,
+  ALERT_SEVERITIES,
+};
